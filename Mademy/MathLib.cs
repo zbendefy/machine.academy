b936e35ee81115905341fd058adef51fe969b659
Bendefy Zoltán <agnu17@gmail.com>
2019-04-26 18:44:55 +0200

    separated opencl specific code into computeframework

--- Mademy/MathLib.cs
+++ Mademy/MathLib.cs
@@ -1,179 +1,36 @@
 ﻿using Mademy;
+using Mademy.OpenCL;
 using OpenCL.Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Mademy.OpenCL.ComputeFramework;
 
 namespace Mademy
 {
     public class MathLib
     {
-        private class MemoryAllocation
-        {
-            public IMem buffer;
-            public int bufferSizeInBytes;
-            public MemFlags flags;
-
-            public MemoryAllocation(IMem buffer, int size, MemFlags flags)
-            {
-                this.buffer = buffer;
-                this.bufferSizeInBytes = size;
-                this.flags = flags;
-            }
-
-            public static MemoryAllocation CreateMemoryAllocation(Context clContext, int byteSize, MemFlags flags, IntPtr data)
-            {
-                ErrorCode err;
-                var buffer = Cl.CreateBuffer(clContext, flags, new IntPtr(byteSize), data, out err);
-                return new MemoryAllocation(buffer, byteSize, flags);
-            }
-
-            internal void Release()
-            {
-                Cl.ReleaseMemObject(buffer);
-            }
-        }
-
-        private static string calcLayerKernel = "calcLayer";
-
-        private object lockObj = new object();
-        Context clContext;
-        ComputeDevice clDevice = null;
-        CommandQueue commandQueue;
-        Program clProgram;
-        Dictionary<String, Kernel> kernels = new Dictionary<string, Kernel>();
-        bool hasClInitialized = false;
-        List<MemoryAllocation> freeMemoryAllocations = new List<MemoryAllocation>();
-        List<MemoryAllocation> usedMemoryAllocations = new List<MemoryAllocation>();
+        private ComputeFramework computeFramework = null;
+        private static readonly string calcLayerKernel = "calcLayer";
 
         public MathLib(ComputeDevice clDevice = null)
         {
-            this.clDevice = clDevice;
-            InitCL();
+            if ( clDevice != null)
+                computeFramework = new ComputeFramework(clDevice, new string[] { CLSourceProvider.ReadSourceFile() }, new string[] { calcLayerKernel } , "-cl-finite-math-only -Werror");
         }
 
-        ~MathLib() { CleanupCLResources(); }
-
-        internal void FlushWorkingCache()
-        {
-            foreach (var item in freeMemoryAllocations)
-                item.Release();
-            freeMemoryAllocations.Clear();
-
-            foreach (var item in usedMemoryAllocations)
-                item.Release();
-            usedMemoryAllocations.Clear();
-        }
-
-        private void CleanupCLResources()
-        {
-            if (hasClInitialized)
-            {
-                FlushWorkingCache();
-
-                foreach (var item in kernels)
-                {
-                    Cl.ReleaseKernel(item.Value);
-                }
-                kernels.Clear();
-
-                Cl.ReleaseProgram(clProgram);
-                Cl.ReleaseCommandQueue(commandQueue);
-                Cl.ReleaseContext(clContext);
-
-                hasClInitialized = false;
-            }
-        }
+        private bool HasComputeFramework() { return computeFramework != null; }
 
         public MathLib Clone()
         {
-            return new MathLib(this.clDevice);
-        }
-
-        private MemoryAllocation GetMemoryFor(int requiredSizeInBytes, MemFlags flags, IntPtr data)
-        {
-            MemoryAllocation candidate = null;
-            int bestMatchingSize = int.MaxValue;
-            int itemToSwap = -1;
-
-            for (int i = 0; i < freeMemoryAllocations.Count; i++)
-            {
-                var item = freeMemoryAllocations[i];
-                if (item.flags == flags && item.bufferSizeInBytes >= requiredSizeInBytes && item.bufferSizeInBytes < bestMatchingSize) //Select the smallest sufficient memory allocation from our allocations
-                {
-                    bestMatchingSize = item.bufferSizeInBytes;
-                    candidate = item;
-                    itemToSwap = i;
-                    if (item.bufferSizeInBytes == requiredSizeInBytes)
-                        break;
-                }
-            }
-
-            if (candidate == null)
-            {
-                candidate = MemoryAllocation.CreateMemoryAllocation(clContext, requiredSizeInBytes, flags, data);
-                usedMemoryAllocations.Add(candidate);
-            }
-            else
-            {
-                freeMemoryAllocations.RemoveAt(itemToSwap);
-                usedMemoryAllocations.Add(candidate);
-                if (flags.HasFlag(MemFlags.CopyHostPtr))
-                {
-                    Event e;
-                    Cl.EnqueueWriteBuffer(commandQueue, candidate.buffer, Bool.False, IntPtr.Zero, new IntPtr(requiredSizeInBytes), data, 0, null, out e);
-                }
-            }
-
-            return candidate;
-        }
-
-        void UnuseMemoryAllocations()
-        {
-            foreach (var item in usedMemoryAllocations)
-            {
-                freeMemoryAllocations.Add(item);
-            }
-            usedMemoryAllocations.Clear();
-        }
-
-        private void InitCL()
-        {
-            lock (lockObj)
-            {
-                if (!hasClInitialized && clDevice != null)
-                {
-                    ErrorCode err;
-                    var devicesArray = new Device[] { clDevice.GetDevice() };
-                    clContext = Cl.CreateContext(null, 1, devicesArray, null, IntPtr.Zero, out err);
-                    if (err != ErrorCode.Success) throw new Exception("Failed to create context! " + err.ToString());
-
-                    commandQueue = Cl.CreateCommandQueue(clContext, clDevice.GetDevice(), CommandQueueProperties.None, out err);
-                    if (err != ErrorCode.Success) throw new Exception("Failed to create command queue! " + err.ToString());
-
-                    clProgram = Cl.CreateProgramWithSource(clContext, 1, new String[] { CLSourceProvider.ReadSourceFile() }, null, out err);
-                    if (err != ErrorCode.Success) throw new Exception("Failed to create program! " + err.ToString());
-
-                    err = Cl.BuildProgram(clProgram, 1, new Device[] { clDevice.GetDevice() }, "-cl-finite-math-only -Werror", null, IntPtr.Zero);
-                    if (err != ErrorCode.Success)
-                    {
-                        var infoBuffer = Cl.GetProgramBuildInfo(clProgram, clDevice.GetDevice(), ProgramBuildInfo.Log, out err);
-                        throw new Exception("Failed to build program! " + err.ToString() + " " + infoBuffer.ToString());
-                    }
-
-                    kernels[calcLayerKernel] = Cl.CreateKernel(clProgram, calcLayerKernel, out err);
-                    if (err != ErrorCode.Success) throw new Exception("Failed to create compute kernel! " + err.ToString());
-
-                    hasClInitialized = true;
-                }
-            }
+            return new MathLib(HasComputeFramework() ? computeFramework.GetOpenCLDevice() : null);
         }
 
         internal float[] CalculateLayer(float[,] weightMx, float[] bias, float[] prevActivations, IActivationFunction sigmoidFunction)
         {
-            if (!hasClInitialized) //CPU fallback
+            if (!HasComputeFramework()) //CPU fallback
             {
                 float[] ret = new float[weightMx.GetLength(0)];
                 for (int m = 0; m < weightMx.GetLength(0); m++)
@@ -198,41 +55,46 @@ namespace Mademy
             {
                 fixed (float* weightArrayPtr = weightMx)
                 {
-                    mem_param_weightMx = GetMemoryFor(weightMx.Length * 4, MemFlags.ReadOnly | MemFlags.CopyHostPtr, new IntPtr(weightArrayPtr));
+                    mem_param_weightMx = computeFramework.GetMemoryFor(weightMx.Length * 4, MemFlags.ReadOnly | MemFlags.CopyHostPtr, new IntPtr(weightArrayPtr));
                 }
                 fixed (float* biasPtr = bias)
                 {
-                    mem_param_bias = GetMemoryFor(bias.Length * 4, MemFlags.ReadOnly | MemFlags.CopyHostPtr, new IntPtr(biasPtr));
+                    mem_param_bias = computeFramework.GetMemoryFor(bias.Length * 4, MemFlags.ReadOnly | MemFlags.CopyHostPtr, new IntPtr(biasPtr));
                 }
                 fixed (float* prevActivationPtr = prevActivations)
                 {
-                    mem_param_prevActivation = GetMemoryFor(prevActivations.Length * 4, MemFlags.ReadOnly | MemFlags.CopyHostPtr, new IntPtr(prevActivationPtr));
+                    mem_param_prevActivation = computeFramework.GetMemoryFor(prevActivations.Length * 4, MemFlags.ReadOnly | MemFlags.CopyHostPtr, new IntPtr(prevActivationPtr));
                 }
 
                 int[] configParams = new int[] { /*rows: */weightMx.GetLength(0), /*cols: */weightMx.GetLength(1), /*ApplySigmoid*/ sigmoidFunction.GetOpenCLFunctionId() };
                 fixed (int* configPtr = configParams)
                 {
-                    mem_param_config = GetMemoryFor(configParams.Length * 4, MemFlags.ReadOnly | MemFlags.CopyHostPtr, new IntPtr(configPtr));
+                    mem_param_config = computeFramework.GetMemoryFor(configParams.Length * 4, MemFlags.ReadOnly | MemFlags.CopyHostPtr, new IntPtr(configPtr));
                 }
-                mem_param_output = GetMemoryFor(matrixRows * 4, MemFlags.WriteOnly, IntPtr.Zero);
+                mem_param_output = computeFramework.GetMemoryFor(matrixRows * 4, MemFlags.WriteOnly, IntPtr.Zero);
             }
 
-            var kernel = kernels[calcLayerKernel];
-            Cl.SetKernelArg(kernel, 0, mem_param_weightMx.buffer);
-            Cl.SetKernelArg(kernel, 1, mem_param_bias.buffer);
-            Cl.SetKernelArg(kernel, 2, mem_param_prevActivation.buffer);
-            Cl.SetKernelArg(kernel, 3, mem_param_config.buffer);
-            Cl.SetKernelArg(kernel, 4, mem_param_output.buffer);
+            computeFramework.SetKernelArg(calcLayerKernel, 0, mem_param_weightMx);
+            computeFramework.SetKernelArg(calcLayerKernel, 1, mem_param_bias);
+            computeFramework.SetKernelArg(calcLayerKernel, 2, mem_param_prevActivation);
+            computeFramework.SetKernelArg(calcLayerKernel, 3, mem_param_config);
+            computeFramework.SetKernelArg(calcLayerKernel, 4, mem_param_output);
 
-            Event ev;
             int localWorkgroupSize = 32;
             int globalWorkSize = (matrixRows % localWorkgroupSize == 0) ? matrixRows : (matrixRows + (localWorkgroupSize - (matrixRows % localWorkgroupSize)));
-            Cl.EnqueueNDRangeKernel(commandQueue, kernel, 1, null, new IntPtr[] { new IntPtr(globalWorkSize) }, new IntPtr[] { new IntPtr(localWorkgroupSize) }, 0, null, out ev);
+            computeFramework.EnqueueKernel(calcLayerKernel, new IntPtr[] { new IntPtr(globalWorkSize) }, new IntPtr[] { new IntPtr(localWorkgroupSize) });
 
             float[] output = new float[matrixRows];
-            Cl.EnqueueReadBuffer(commandQueue, mem_param_output.buffer, Bool.True, 0, matrixRows, output, 0, null, out ev);
 
-            UnuseMemoryAllocations();
+            unsafe
+            {
+                fixed (float* outputPtr = output)
+                {
+                    computeFramework.ReadBuffer(mem_param_output, true, IntPtr.Zero, new IntPtr(matrixRows * 4), new IntPtr(outputPtr));
+                }
+            }
+
+            computeFramework.UnuseMemoryAllocations();
 
             return output;
         }
@@ -303,6 +165,12 @@ namespace Mademy
             gamma_k_vector = newGammak;
         }
 
+        internal void FlushWorkingCache()
+        {
+            if ( HasComputeFramework())
+                computeFramework.FlushWorkingCache();
+        }
+
         /// <summary>
         /// Runs backpropagation
         /// </summary>
@@ -316,7 +184,7 @@ namespace Mademy
             //Backpropagation
             var ret = Utils.CreateGradientVector(network);
 
-            if (!hasClInitialized || true) //CPU fallback
+            if (!HasComputeFramework() || true) //CPU fallback
             {
                 for (int i = trainingDataBegin; i < trainingDataEnd; i++)
                 {
---
+++ Mademy/OpenCL/ComputeFramework.cs
@@ -0,0 +1,195 @@
+﻿using OpenCL.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mademy.OpenCL
+{
+    internal class ComputeFramework
+    {
+        public class MemoryAllocation
+        {
+            public IMem buffer;
+            public int bufferSizeInBytes;
+            public MemFlags flags;
+
+            public MemoryAllocation(IMem buffer, int size, MemFlags flags)
+            {
+                this.buffer = buffer;
+                this.bufferSizeInBytes = size;
+                this.flags = flags;
+            }
+
+            public static MemoryAllocation CreateMemoryAllocation(Context clContext, int byteSize, MemFlags flags, IntPtr data)
+            {
+                ErrorCode err;
+                var buffer = Cl.CreateBuffer(clContext, flags, new IntPtr(byteSize), data, out err);
+                return new MemoryAllocation(buffer, byteSize, flags);
+            }
+
+            internal void Release()
+            {
+                Cl.ReleaseMemObject(buffer);
+            }
+        }
+
+        private object lockObj = new object();
+        private Context clContext;
+        private ComputeDevice clDevice = null;
+        private CommandQueue commandQueue;
+        private Program clProgram;
+        private Dictionary<String, Kernel> kernels = new Dictionary<string, Kernel>();
+        private bool hasClInitialized = false;
+        private List<MemoryAllocation> freeMemoryAllocations = new List<MemoryAllocation>();
+        private List<MemoryAllocation> usedMemoryAllocations = new List<MemoryAllocation>();
+
+        public ComputeDevice GetOpenCLDevice() { return clDevice; }
+
+        public ComputeFramework(ComputeDevice clDevice, String[] kernelSource, String[] kernelNames, string compileArguments)
+        {
+            this.clDevice = clDevice;
+            InitCL(kernelSource, kernelNames, compileArguments);
+        }
+
+        ~ComputeFramework() { CleanupCLResources(); }
+
+        public void FlushWorkingCache()
+        {
+            foreach (var item in freeMemoryAllocations)
+                item.Release();
+            freeMemoryAllocations.Clear();
+
+            foreach (var item in usedMemoryAllocations)
+                item.Release();
+            usedMemoryAllocations.Clear();
+        }
+
+        public void CleanupCLResources()
+        {
+            if (hasClInitialized)
+            {
+                FlushWorkingCache();
+
+                foreach (var item in kernels)
+                {
+                    Cl.ReleaseKernel(item.Value);
+                }
+                kernels.Clear();
+
+                Cl.ReleaseProgram(clProgram);
+                Cl.ReleaseCommandQueue(commandQueue);
+                Cl.ReleaseContext(clContext);
+
+                hasClInitialized = false;
+            }
+        }
+
+        public bool IsInitialized() { return hasClInitialized; }
+
+        public MemoryAllocation GetMemoryFor(int requiredSizeInBytes, MemFlags flags, IntPtr data)
+        {
+            if (!IsInitialized())
+                return null;
+
+            MemoryAllocation candidate = null;
+            int bestMatchingSize = int.MaxValue;
+            int itemToSwap = -1;
+
+            for (int i = 0; i < freeMemoryAllocations.Count; i++)
+            {
+                var item = freeMemoryAllocations[i];
+                if (item.flags == flags && item.bufferSizeInBytes >= requiredSizeInBytes && item.bufferSizeInBytes < bestMatchingSize) //Select the smallest sufficient memory allocation from our allocations
+                {
+                    bestMatchingSize = item.bufferSizeInBytes;
+                    candidate = item;
+                    itemToSwap = i;
+                    if (item.bufferSizeInBytes == requiredSizeInBytes)
+                        break;
+                }
+            }
+
+            if (candidate == null)
+            {
+                candidate = MemoryAllocation.CreateMemoryAllocation(clContext, requiredSizeInBytes, flags, data);
+                usedMemoryAllocations.Add(candidate);
+            }
+            else
+            {
+                freeMemoryAllocations.RemoveAt(itemToSwap);
+                usedMemoryAllocations.Add(candidate);
+                if (flags.HasFlag(MemFlags.CopyHostPtr))
+                {
+                    Event e;
+                    Cl.EnqueueWriteBuffer(commandQueue, candidate.buffer, Bool.False, IntPtr.Zero, new IntPtr(requiredSizeInBytes), data, 0, null, out e);
+                }
+            }
+
+            return candidate;
+        }
+
+        public void UnuseMemoryAllocations()
+        {
+            foreach (var item in usedMemoryAllocations)
+            {
+                freeMemoryAllocations.Add(item);
+            }
+            usedMemoryAllocations.Clear();
+        }
+
+        public void SetKernelArg(string kernelName, uint idx, MemoryAllocation mem)
+        {
+            Cl.SetKernelArg(kernels[kernelName], idx, mem.buffer);
+        }
+
+        public void ReadBuffer(MemoryAllocation mem, bool isBlocking, IntPtr offset, IntPtr lengthInBytes, IntPtr output)
+        {
+            Event ev;
+            Cl.EnqueueReadBuffer(commandQueue, mem.buffer, isBlocking ? Bool.True : Bool.False, offset, lengthInBytes, output, 0, null, out ev);
+        }
+
+        public void EnqueueKernel(string kernelName, IntPtr[] globalWorkSize, IntPtr[] localWorkSize)
+        {
+            Event ev;
+            Cl.EnqueueNDRangeKernel(commandQueue, kernels[kernelName], 1, null, globalWorkSize, localWorkSize, 0, null, out ev);
+        }
+
+        private void InitCL(String[] kernelSource, String[] kernelNames, string compileArguments)
+        {
+            lock (lockObj)
+            {
+                if (!hasClInitialized && clDevice != null)
+                {
+                    ErrorCode err;
+                    var devicesArray = new Device[] { clDevice.GetDevice() };
+                    clContext = Cl.CreateContext(null, 1, devicesArray, null, IntPtr.Zero, out err);
+                    if (err != ErrorCode.Success) throw new Exception("Failed to create context! " + err.ToString());
+
+                    commandQueue = Cl.CreateCommandQueue(clContext, clDevice.GetDevice(), CommandQueueProperties.None, out err);
+                    if (err != ErrorCode.Success) throw new Exception("Failed to create command queue! " + err.ToString());
+
+                    clProgram = Cl.CreateProgramWithSource(clContext, 1, kernelSource.ToArray(), null, out err);
+                    if (err != ErrorCode.Success) throw new Exception("Failed to create program! " + err.ToString());
+
+                    err = Cl.BuildProgram(clProgram, 1, new Device[] { clDevice.GetDevice() }, compileArguments, null, IntPtr.Zero);
+                    if (err != ErrorCode.Success)
+                    {
+                        var infoBuffer = Cl.GetProgramBuildInfo(clProgram, clDevice.GetDevice(), ProgramBuildInfo.Log, out err);
+                        throw new Exception("Failed to build program! " + err.ToString() + " " + infoBuffer.ToString());
+                    }
+
+                    foreach (var item in kernelNames)
+                    {
+                        kernels[item] = Cl.CreateKernel(clProgram, item, out err);
+                    }
+
+                    if (err != ErrorCode.Success) throw new Exception("Failed to create compute kernel! " + err.ToString());
+
+                    hasClInitialized = true;
+                }
+            }
+        }
+
+    }
+}
