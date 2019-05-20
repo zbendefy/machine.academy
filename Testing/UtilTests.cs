e0487ebed63fcff3660907067c11c4461d533634
Zoltan Bendefy <zbendefy@gmail.com>
2019-05-20 21:26:22 +0200

    added placeholder test file

---
+++ Testing/UtilTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class UtilTests
+    {
+        [TestMethod]
+        public void TestListShuffle()
+        {
+            Assert.Fail("unimplemented test");
+        }
+    }
+}
