eede31244e340a7a72b1fe94b7d7b809ba03cca8
Bendefy Zoltán <agnu17@gmail.com>
2019-09-01 10:04:36 +0200

    fixed naming

--- docs/WebApps/Evo/drawing.js
+++ docs/WebApps/Evo/drawing.js
@@ -36,7 +36,7 @@ class EvoDrawing
         this.outlierDelta = 20;
 
         this.drawFpsTarget = 30;
-        this.drawFrmeTimeTarget  = 1 / this.drawFpsTarget;
+        this.drawFrameTimeTargetS  = 1 / this.drawFpsTarget;
         this.drawingTimer = 0;
 
         this.currentSessionTimer = 0;
@@ -141,7 +141,7 @@ class EvoDrawing
         this.drawingTimer -= this.frameTimeS;
         if (this.drawingTimer <= 0){
             this.Draw();
-            this.drawingTimer = Math.max( this.drawingTimer + this.drawFrmeTimeTarget, -this.drawFrmeTimeTarget );
+            this.drawingTimer = Math.max( this.drawingTimer + this.drawFrameTimeTargetS, -this.drawFrameTimeTargetS );
         }
     }
 
